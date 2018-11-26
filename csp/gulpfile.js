a9196b741fc1a012bfe00dd3f1ac7171be5a6090
Joao Lourenco <joao.lourenco@rokt.com>
2018-11-26 15:53:56 +1100

    Added description to SRI examples in body

--- csp/gulpfile.js
+++ csp/gulpfile.js
@@ -16,12 +16,12 @@ function serve() {
             case "/sri":
                 headers["Content-Type"] = "text/html";
                 headers["Content-Security-Policy"] = `default-src 'none'; script-src 'sha256-${crypto.createHash("sha256").update(alert).digest("base64")}'`;
-                content = `<html><head><script src='alert.js' integrity='sha256-${crypto.createHash("sha256").update(alert).digest("base64")}'></script></head></head><body>Hello world</body></html>`;
+                content = `<html><head><script src='alert.js' integrity='sha256-${crypto.createHash("sha256").update(alert).digest("base64")}'></script></head></head><body>SRI works here and allows the script to execute.</body></html>`;
                 break;
             case "/sri-eval":
                 headers["Content-Type"] = "text/html";
                 headers["Content-Security-Policy"] = `default-src 'none'; script-src 'sha256-${crypto.createHash("sha256").update(alertEval).digest("base64")}'`;
-                content = `<html><head><script src='alert-eval.js' integrity='sha256-${crypto.createHash("sha256").update(alertEval).digest("base64")}'></script></head></head><body>Hello world</body></html>`;
+                content = `<html><head><script src='alert-eval.js' integrity='sha256-${crypto.createHash("sha256").update(alertEval).digest("base64")}'></script></head></head><body>SRI works here, but due to the eval call, the script execution is blocked.</body></html>`;
                 break;
             case "/alert.js":
                 headers["Content-Type"] = "text/javascript";
